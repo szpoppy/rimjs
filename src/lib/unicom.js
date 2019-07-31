d9f40bfce5668bda9a3e3d9a8049cea99bec5692
张钰峰 <zyf47976@ly.com>
2019-07-31 14:38:45 +0800

    优化注解，修复全局订阅问题以及一些多余代码删除

--- src/lib/unicom.js
+++ src/lib/unicom.js
@@ -1,4 +1,5 @@
 (function(global, factory) {
+    // UMD 加载方案
     if (typeof exports === "object" && typeof module !== "undefined") {
         module.exports = factory();
         return;
@@ -19,10 +20,10 @@
 
     // 目前所有实例化的 unicom通讯 方便做 订阅者和发布者
 
-    // #id
+    // #id 存放id的unicom对象
     let unicomGroupByID = {};
     function updateUnicomGroupByID(target, newId, oldId) {
-        // 更新
+        // 更新 id 触发更新
         if (oldId && unicomGroupByID[oldId] == target) {
             delete unicomGroupByID[oldId];
         }
@@ -31,9 +32,10 @@
         }
     }
 
-    // @name
+    // @name 存放name的数组对象
     let unicomGroupByName = {};
     function addUnicomGroupByNameOne(target, name) {
+        // 加入一个name
         let unicoms = unicomGroupByName[name];
         if (!unicoms) {
             unicoms = unicomGroupByName[name] = [];
@@ -43,6 +45,7 @@
         }
     }
     function updateUnicomGroupByName(target, newName, oldName) {
+        // 某个unicom对象更新 name
         if (oldName) {
             // 移除所有旧的
             oldName.forEach(function(name) {
@@ -64,7 +67,7 @@
         }
     }
 
-    // @all
+    // @all 所有的unicom对象集合，发布指令是待用
     let unicomGroup = [];
     function addUnicomGroup(target) {
         // 添加
@@ -78,22 +81,27 @@
         }
     }
 
-    // 事件 参数
+    // 发布指令时产生的事件的类
     class UnicomEvent {
         constructor(from, args) {
+            // 来自
             this.from = from;
+            // 目标绑定的对象，vue中代表vue的实例
             this.target = from.target;
+            // 第一号数据
             this.data = args[0];
+            // 多个数据 使用 $index 表示
             args.forEach((arg, index) => {
                 this["$" + (index + 1)] = arg;
             });
         }
     }
 
-    // 单列模式触发
+    // 发布事件
     function emitAll(self, type, target, instruct, args) {
         let targetUnicom = [];
         if (type == "#") {
+            // 目标唯一
             let one = unicomGroupByID[target];
             if (!instruct) {
                 // 只是获取
@@ -103,6 +111,7 @@
                 targetUnicom.push(one);
             }
         } else if (type == "@") {
+            // 目标是个分组
             let group = unicomGroupByName[target];
             if (!instruct) {
                 // 只是获取
@@ -116,6 +125,7 @@
         }
         let uniEvent = new UnicomEvent(self, args);
         targetUnicom.forEach(function(emit) {
+            // 每个都触发一遍
             emit.emit(instruct, uniEvent, ...args);
         });
         return uniEvent;
@@ -127,7 +137,7 @@
         for (let i = 0; i < monitorArr.length; i += 1) {
             let [type, target, callback] = monitorArr[i];
             if ((type == "#" && that.id == target) || (type == "@" && that.group.indexOf(target) > -1)) {
-                // 运行回调
+                // 运行监控回调
                 callback(that);
             }
         }
@@ -136,16 +146,21 @@
     // 通讯基础类
     class Unicom {
         constructor({ id, group, target = null } = {}) {
-            // 克隆一份 事件
-            this._instruct_ = {};
             let _instruct_ = this._instruct_;
+            this._instruct_ = {};
             if (_instruct_) {
+                // 克隆一份 事件
+                // 通过 Unicom.prototype.on() 会把事件写入 Unicom.prototype._instruct_
+                // 所以要重写，防止全局冲突
                 for (let n in _instruct_) {
                     this._instruct_[n] = [].push(..._instruct_[n]);
                 }
             }
 
+            // 绑定的目标对象
             this.target = target;
+
+            // 分组
             this.group = [];
 
             if (id) {
@@ -156,9 +171,10 @@
                 this.setGroup(group);
             }
 
+            // 将实例加入到单例的队列
             addUnicomGroup(this);
 
-            // 运行延后执行，作为监控
+            // 查找监控中是否被监控中
             this.monitorBack();
         }
 
@@ -168,6 +184,8 @@
             this._monitor_back_ = setTimeout(() => {
                 monitorExec(this);
             }, 1);
+
+            return this;
         }
 
         // 监听目标创建
@@ -175,6 +193,7 @@
             let type = instruct.slice(0, 1);
             let target = instruct.slice(1, instruct.length);
             monitorArr.push([type, target, callback, this]);
+            return this;
         }
 
         // 销毁监听
@@ -188,10 +207,12 @@
 
                 i += 1;
             }
+            return this;
         }
 
         // 销毁
         destroy() {
+            // 销毁队列
             removeUnicomGroup(this);
 
             // 移除
@@ -240,12 +261,7 @@
             return !!(instruct && instruct.length > 0);
         }
 
-        /**
-         * 绑定事件
-         * @param type 事件名称
-         * @param fun 事件方法
-         * @returns {EventEmitter}
-         */
+        // 订阅消息
         on(type, fun) {
             let instruct = this._instruct_ || (this._instruct_ = {});
             instruct[type] || (instruct[type] = []);
@@ -253,12 +269,7 @@
             return this;
         }
 
-        /**
-         * 移除事件
-         * @param type 事件名称
-         * @param fun 事件方法
-         * @returns {EventEmitter}
-         */
+        // 移除订阅
         off(type, fun) {
             let instruct = this._instruct_;
             if (instruct) {
@@ -282,10 +293,7 @@
             return this;
         }
 
-        /**
-         * 触发事件
-         * @param {String} type 事件名称
-         */
+        // 触发订阅
         emit(query, ...args) {
             let data = args[0];
             if (data && data.constructor == UnicomEvent) {
@@ -313,6 +321,7 @@
     let unicomInstalled = false;
     Unicom.install = function(vue, { name = "unicom", unicomName, unicomId, unicomEmit, unicomClass } = {}) {
         if (unicomInstalled) {
+            // 防止重复install
             return;
         }
         unicomInstalled = true;
@@ -343,7 +352,7 @@
             return unicomData.initGroup.concat(names);
         }
 
-        // 全局混入
+        // 全局混入 vue
         vue.mixin({
             props: {
                 // 命名
@@ -375,7 +384,9 @@
             beforeCreate() {
                 // 屏蔽不需要融合的 节点
                 let isIgnore = !this.$vnode || /-transition$/.test(this.$vnode.tag);
+                // unicomData 数据存放
                 let unicomData = (this._unicom_data_ = {
+                    // 不需要，忽略
                     isIgnore
                 });
                 if (isIgnore) {
@@ -391,11 +402,12 @@
             created() {
                 let unicomData = this._unicom_data_;
                 if (unicomData.isIgnore) {
+                    // 忽略
                     return;
                 }
 
                 // 初始化
-                let self = (unicomData.self = new Unicom({ target: this, id: this[unicomIdName], group: getGroup(this), subs: unicomData.channels }));
+                let self = (unicomData.self = new Unicom({ target: this, id: this[unicomIdName], group: getGroup(this) }));
 
                 // 订阅事件
                 let subChannels = unicomData.channels;
@@ -409,8 +421,10 @@
             destroyed() {
                 let unicomData = this._unicom_data_;
                 if (unicomData.isIgnore) {
+                    // 忽略
                     return;
                 }
+                // 销毁 unicom 对象
                 unicomData.self.destroy();
             }
         });
