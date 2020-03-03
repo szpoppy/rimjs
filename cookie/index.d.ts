66704a765bf5fae0ed5441b2a7cf821e8e504ab8
张钰峰 <zyf47976@ly.com>
2020-03-03 09:53:43 +0800

    build

--- cookie/index.d.ts
+++ cookie/index.d.ts
@@ -1,3 +1,4 @@
+declare type TExpiration = number | string | Date;
 /**
  * 获取cookie
  * @param {String} key
@@ -8,10 +9,11 @@ export declare function getCookie(key: string): string;
  * @param {String} key
  * @param {String} value
  * @param {Number|String|Date} expiration 0:进程，-1:永久，数字:天数，字符串，日期
+ * @param {Number|String|Date} expiration 1d 1天 1h 1小时 1m 1分钟
  * @param {String} path 文档路径
  * @param {String} domain 域名，可以设置主域名
  */
-export declare function setCookie(key: string, value: string, expiration: number | string | Date, path?: string, domain?: string): void;
+export declare function setCookie(key: string, value: string, expiration?: TExpiration, path?: string, domain?: string): void;
 /**
  * 移除cookie
  * @param {*} key
@@ -19,9 +21,20 @@ export declare function setCookie(key: string, value: string, expiration: number
  * @param {*} domain 域名，可以设置主域名
  */
 export declare function removeCookie(key: string, path?: string, domain?: string): void;
-declare const _default: {
-    getItem: typeof getCookie;
-    setItem: typeof setCookie;
-    removeItem: typeof removeCookie;
-};
-export default _default;
+export default class Cookie {
+    expiration?: Date;
+    path?: string;
+    domain?: string;
+    constructor({ expiration, path, domain }?: {
+        expiration: TExpiration;
+        path?: string;
+        domain?: string;
+    });
+    static getItem: typeof getCookie;
+    getItem(key: string): string;
+    static setItem: typeof setCookie;
+    setItem(key: string, value: string): void;
+    static removeItem: typeof removeCookie;
+    removeItem(key: string): void;
+}
+export {};
